d509a5494daa8078cd1b02835f0a46edbe181ca0
vikvang <vikvang01@gmail.com>
2024-10-24 19:32:48 -0400

    models and db

--- backend/tedbackend/api/models.py
+++ backend/tedbackend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ButtonPress(models.Model):
+    button_id = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=255)
+
+class SpeechSimulation(models.Model):
+    text = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    response = models.CharField(max_length=255)
